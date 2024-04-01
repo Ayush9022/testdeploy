960eba74e713f4bed53599540d3d7c240fc0b70e
Ayush <ayushgodbole2604@gmail.com>
2024-04-01 09:30:23 +0530

    added icecream commit

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get("/about", (req, res) => {
   res.send("we are pro developers");
 });
 
+app.get("/icecream", (req, res) => {
+  res.send("congrats , icecream for you !!");
+});
+
 app.listen(4500, () => {
   console.log("running");
 });
